3b925714a8cdbee2c97e82a23791c88792761974
Nikhil79922 <ns94301918@gmail.com>
2025-02-18 14:29:18 +0530

    fix

--- src/components/Control/Control_header.jsx
+++ src/components/Control/Control_header.jsx
@@ -2,12 +2,9 @@ import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import pencil from "../../assets/svg/pencil.svg";
 import { useSelector } from "react-redux";
-
 function Control_header() {
   const isSlideOpen = useSelector((state) => state.sidebar.isSlideOpen);
-  const location = useLocation(); // Extract current path
-
-  // Map paths to display names
+  const location = useLocation();
   const pathToLabel = {
     "/dashboard/control/orden": "Orden",
     "/dashboard/control/scrap": "Scrap",
@@ -15,15 +12,10 @@ function Control_header() {
     "/dashboard/control/consumos": "Consumos",
     "/dashboard/control/etiquetas": "Etiquetas",
   };
-
-  // Track the active tab name
   const [value, setValue] = useState(pathToLabel[location.pathname] || "Orden");
-
-  // Update value when location changes
   useEffect(() => {
     setValue(pathToLabel[location.pathname] || "Orden");
   }, [location.pathname]);
-
   return (
     <div className={`ml-[20px] flex h-[100px] justify-between w-[96vw] ${isSlideOpen ? `lg:w-[81.6vw]` : `lg:w-[96vw]`}`}>
       <div className="flex gap-[2px] sm:gap-[6px] md:gap-[9px] lg:gap-[12px] items-center mt-4 mb-6">
@@ -32,7 +24,6 @@ function Control_header() {
         </h2>
         <img src={pencil} className="bg-gray-4 h-[16px] ml-[13px] relative bottom-[1px] cursor-pointer" />
       </div>
-      {/* Navigation Links */}
       <div className="flex gap-[2px] h-[50px] sm:gap-3 md:gap-4 lg:gap-6 items-center mt-4 mb-6">
         {[
           { to: "/dashboard/control/orden", text: "Orden" },
@@ -42,19 +33,8 @@ function Control_header() {
           { to: "/dashboard/control/etiquetas", text: "Etiquetas" },
         ].map(({ to, text }) => (
           <Link
-            key={to}
-            to={to}
-            className={`text-[10px] md:text-[15.5px] lg:text-[16.5px] font-inter font-[600] px-2 py-1 rounded-sm transition-all box-border ${
-              location.pathname === to ? "text-blue-1 border-b-2 border-blue-1 " : "text-gray-2"
-            }`}
-            
-            
-          >
-            {text}
-          </Link>
-        ))}
+            key={to} to={to} className={`text-[10px] md:text-[15.5px] lg:text-[16.5px] font-inter font-[600] px-2 py-1 rounded-sm transition-all box-border ${location.pathname === to ? "text-blue-1 border-b-2 border-blue-1 " : "text-gray-2"}`} > {text} </Link>))}
       </div>
-    </div>
-  );
+    </div>);
 }
 export default Control_header;
