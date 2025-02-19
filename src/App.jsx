86df91fe207662ac0d0968b227a421f2c627a88d
Nikhil79922 <ns94301918@gmail.com>
2025-02-19 17:13:46 +0530

    fix scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router } from "react-router-dom";
-import AppRoutes from "./routes/AppRoute.jsx"; 
+import AppRoutes from "./routes/AppRoute.jsx";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <AppRoutes />
     </Router>
   );
