851562566e1c3a5df2dbcd220ae4c9d246c00c0e
Amrutkar191 <bhaveshamrutkar2003@gmail.com>
2025-02-22 12:30:01 +0530

    added remember me & log out

--- src/components/authentication/Login.jsx
+++ src/components/authentication/Login.jsx
@@ -11,15 +11,23 @@ export default function LoginPage() {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
   const { register, handleSubmit, formState: { errors } } = useForm();
+  const [rememberMe, setRememberMe] = useState(false);
+
 
   const onSubmit = async (data) => {
-    
+
     try {
-      const response = await axios.post("http://localhost:8000/auth/login", data);
-    
+      const response = await axios.post("http://localhost:8000/auth/login", data, rememberMe);
+
       if (response.status === 200 && response.data.Data.token) {
-        localStorage.setItem("token", response.data.Data.token);
-    
+        if(rememberMe)
+        {
+          localStorage.setItem("token", response.data.Data.token);
+        }
+        else{
+          sessionStorage.setItem("token", response.data.Data.token);
+        }
+
         // Show success toast and navigate to dashboard
         toast.success("Login successful!", {
           position: "top-right",
@@ -34,8 +42,8 @@ export default function LoginPage() {
     } finally {
       setLoading(false);
     }
-    
-    
+
+
   };
 
   return (
@@ -48,21 +56,21 @@ export default function LoginPage() {
       {/* Form */}
       <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
 
-        <div className="mb-2 "> 
+        <div className="mb-2 ">
           <label className="block text-left text-gray-1 font-[500] text-[16px] font-inter mb-1">Email</label>
           <input
             type="email"
             placeholder="example@email.com"
             className="relative mb-1 w-full border border-gray-300 placeholder:text-[14px] placeholder:text-gray-5 placeholder:font-[400] placeholder:font-inter rounded-md px-4 py-2 focus:ring-2 focus:ring-blue-400 outline-none"
             {...register("email",
-               {
-              required: "Email is required",
-              pattern: {
-                value: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/,
-                message: "Invalid email format",
-              },
-            }
-          )}
+              {
+                required: "Email is required",
+                pattern: {
+                  value: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/,
+                  message: "Invalid email format",
+                },
+              }
+            )}
           />
           {errors.email && <p className="absolute  text-red-500 text-xs mt-[1px] md:mt-[2px]">{errors.email.message}</p>}
         </div>
@@ -74,21 +82,34 @@ export default function LoginPage() {
             placeholder="At least 8 characters"
             className="relative mb-1 w-full border border-gray-300 placeholder:text-[14px] placeholder:text-gray-5 placeholder:font-[400] placeholder:font-inter rounded-md px-4 py-2 focus:ring-2 focus:ring-blue-400 outline-none"
             {...register("password",
-               {
-              required: "Password is required",
-              minLength: { value: 8, message: "Password must be of at least 8 characters." },
-              pattern: {
-                value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])/,
-                message: "Password must include A-Z, a-z, 0-9 and !@#$%^&*.",
-              },
-            }
-          )}
+              {
+                required: "Password is required",
+                minLength: { value: 8, message: "Password must be of at least 8 characters." },
+                pattern: {
+                  value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])/,
+                  message: "Password must include A-Z, a-z, 0-9 and !@#$%^&*.",
+                },
+              }
+            )}
           />
           {errors.password && <p className=" absolute  text-red-500 text-xs mt-[1px] md:mt-[2px]">{errors.password.message}</p>}
         </div>
 
+        <div className="flex justify-between tems-center" >
+          {/* Remember Me Checkbox */}
+          <div className="flex items-center">
+            <input
+              type="checkbox"
+              id="rememberMe"
+              className="mr-2 cursor-pointer"
+              checked={rememberMe}
+              onChange={(e) => setRememberMe(e.target.checked)}
+            />
+            <label htmlFor="rememberMe" className="text-blue-1 font-inter font-[500] text-sm cursor-pointer">Remember Me</label>
+          </div>
 
-        <div className="text-right">
+
+
           <button
             type="button"
             onClick={() => navigate("/forgot-password")}
@@ -96,7 +117,10 @@ export default function LoginPage() {
           >
             Forgot Password?
           </button>
+
         </div>
+
+
 
 
         <button
